68af314804cba547d0cb56a10d1c713548a522f7
Steven Shatz <sshatzdeveloper@gmail.com>
2024-05-06 16:22:30 -0400

    Display non-functioning Contact Us form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.header("Contact Me")
+
+with st.form('Contact_Form'):
+    email = st.text_input("Your email address")
+    message = st.text_input("Your message")
+    st.form_submit_button("Submit")
